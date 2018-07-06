4217e72cce6ca35d3b551cf1999f9583ebb73d37
Matt Linaberry <matt.linaberry@mrs-electronic.com>
2018-07-06 17:31:55 -0400

    thru 27.

--- src/index.js
+++ src/index.js
@@ -1,18 +1,35 @@
 // IMports!
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
 const API_KEY = 'AIzaSyCy6OzbZqgPIF6aVkWqWqOSsYHqfNfkTQc';
 
+
+
 // Create a new component. It will produce some HTML.
-const App = () => {  // const is ES6 stuff. it's declaring App as a true final constant.
-    return (
+class App extends Component {  // const is ES6 stuff. it's declaring App as a true final constant.+
+    constructor(props) {
+        super(props);
+        this.state = { videos: [] };
+
+        YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+            // now update the state with the new videos
+            this.setState({ videos });  // json == {videos: videos}
+        });
+    }
+    // pass those props!
+    render() {
+        return (
     <div>
         <SearchBar />
+        <VideoList videos={this.state.videos}/> 
     </div>
     );
+    }
 }
 
 // Put it in the DOM!
